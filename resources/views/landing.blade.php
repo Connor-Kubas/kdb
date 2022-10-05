261e03bb6f702b9c7d07a81203a237492148cf72
Connor-Kubas <conman1507@gmail.com>
2022-10-05 05:24:16 -0500

    Reworked views to use layouts and components.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,32 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
+    @section('content')
+        <x-search></x-search>
+    @endsection
 
-        <!-- Google fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-
-        <link rel="icon" href="{{ url('images/favicon.ico') }}">
-
-        <link href="{{ url('css/landing.css')}}" rel="stylesheet">
-
-        <title>KDB</title>
-
-    </head>
-    <body>
-        <div class="container">
-            <div class="text">
-                {{-- <h1>Welcome to Kubas DB</h1> --}}
-
-                <form action="/view" style="background-color: transparent;">
-                    @csrf
-                    <input name="search" id="search" type="text" style="background-color: transparent;" onclick='grow()'>
-                </form>
-            </div>    
-        </div>
-
-    </body>
-</html>
-
-<script src="{{ url('js/search.js') }}"></script>
+@push('script')
+    <script type="text/javascript" src="{{ url('js/search.js') }}"></script>
+@endpush
--- resources/views/movie-view.blade.php
+++ resources/views/movie-view.blade.php
@@ -1,32 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Google fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-
-        <link rel="icon" href="{{ url('images/favicon.ico') }}">
-
-        <link href="{{ url('css/landing.css')}}" rel="stylesheet">
-
-        <title>KDB</title>
-
-    </head>
-    <body>
-        <div class="container">
-            <div class="text">
-                <form action="/view" style="background-color: transparent;">
-                    @csrf
-                    <input name="search" id="search" type="text" style="background-color: transparent;" onclick='grow()' oninput='expandWithText()'>
-                </form>
-            </div>    
-        </div>
-
+@extends('layouts.master')
+    @section('content')
+        <x-search></x-search>
         <img src="{{ url("images/posters/$search") }}" width="400px" style="border: 15px solid black; border-radius: 15px; margin-left: 350px; margin-top: 50px;">
+    @endsection
 
-    </body>
-</html>
-
-<script src="{{ url('js/search.js') }}"></script>
+@push('script')
+    <script type="text/javascript" src="{{ url('js/search.js') }}"></script>
+@endpush
