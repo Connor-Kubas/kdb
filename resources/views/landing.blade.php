973aef071a01fece0ffbcd15b9f1b56fc18beee8
Connor-Kubas <conman1507@gmail.com>
2022-10-04 22:10:03 -0500

    Added landing page.

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Google fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+
+        <link href="{{ url('css/landing.css')}}" rel="stylesheet">
+
+        <title>Kubas DB</title>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="text">
+                <h1>Welcome to Kubas DB</h1>
+
+                <p>We are better than Kino</p>
+            </div>    
+        </div>
+
+    </body>
+</html>
